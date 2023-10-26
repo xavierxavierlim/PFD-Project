311f36bf8f5e5623a4a1a3575b4734b88e25399d
Xavier Lim <s10242016@connect.np.edu.sg>
2023-10-26 15:02:04 +0800

    Add project files.

---
+++ PFD Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PFD_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
