bae648d3e9e543f0e2392ca0ee44615c11851383
Xavier Lim <s10242016@connect.np.edu.sg>
2023-11-01 12:46:10 +0800

    created page to adjust screen (incomplete)

--- PFD Project/Controllers/HomeController.cs
+++ PFD Project/Controllers/HomeController.cs
@@ -63,6 +63,11 @@ namespace PFD_Project.Controllers
             return View();
         }
 
+        public IActionResult AdjustScreen()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
