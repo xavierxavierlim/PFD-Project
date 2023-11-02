d9c5b5964c809afa74aa891f2bc2e4436697476e
GohJiaDong <s10243744@connect.np.edu.sg>
2023-11-02 15:19:22 +0800

    added webcam stream (not draggable)

---
+++ PFD Project/wwwroot/js/streamCam.js
@@ -0,0 +1,39 @@
+﻿var cam = false;
+
+const closeCam = () => {
+    const video = document.getElementById("streamCam");
+    let stream = video.srcObject;
+    var tracks = stream.getVideoTracks();
+    tracks.forEach(track => track.enabled = false);
+
+    const displayContainer = document.getElementById('camContainer');
+    displayContainer.style.display = 'none';
+
+    cam = false;
+};
+const openCam = () => {
+
+    //Initialize video
+    const video = document.getElementById("streamCam");
+
+    // validate video element
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((stream) => {
+                video.srcObject = stream;
+            })
+            .catch(function (error) {
+                console.log("Something went wrong!");
+            });
+    }
+
+    const displayContainer = document.getElementById('camContainer');
+    displayContainer.style.display = 'block';
+
+    cam = true;
+}
+
+function toggleCam() {
+    cam ? closeCam() : openCam();
+}
