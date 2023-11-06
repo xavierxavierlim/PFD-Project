f027f4fbc5e51233cd7944daed9550512e0ee509
GohJiaDong <s10243744@connect.np.edu.sg>
2023-11-06 22:12:38 +0800

    Finished WebCam feature (Persistent + Moveable) Added resources for "Magnify Screen" feature

---
+++ PFD Project/wwwroot/js/resizeScreen.js
@@ -0,0 +1,12 @@
+﻿var popup = false;
+const closeResize = () => {
+    document.getElementById("resizePopUp").style.display = "none";
+    popup = false;
+}
+const openResize = () => {
+    document.getElementById("resizePopUp").style.display = "block";
+    popup = true;
+}
+function toggleResize() {
+    popup ? closeResize() : openResize();
+}
