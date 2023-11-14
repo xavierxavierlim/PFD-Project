cfa3f4227b61b9efa2ea16529ea3cdf8fa3c80b2
Zi Jie Goh <s10247488@connect.np.edu.sg>
2023-11-14 16:42:06 +0800

    Added the TransferConfirmation.cshtml (front-end)

--- PFD Project/Controllers/HomeController.cs
+++ PFD Project/Controllers/HomeController.cs
@@ -94,6 +94,11 @@ namespace PFD_Project.Controllers
             return View();
         }
 
+        public IActionResult TransferConfirmation()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
